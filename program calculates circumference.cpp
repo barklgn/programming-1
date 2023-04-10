b099c9401cefceb1fcc2edf25c0f16dbca4bbb0f
BARKLGN MULUGETA <65805233+barklgn@users.noreply.github.com>
2023-04-10 07:48:03 +0300

    Add files via upload

---
+++ program calculates circumference.cpp
@@ -0,0 +1,22 @@
+﻿﻿/* This code is created by
+Name : BARKLGN MULUGETA
+Id : Ru 2141/14 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+//program that calculates the circumference of a circle
+cout << "the 3rd program calculates the circumference of the circle" << endl;
+float const pi = 3.14;
+float radius,circumference;
+
+cout << "enter the radius of the circle " << endl;
+cin >> radius;
+
+circumference = ((2*pi) * radius);
+cout << "the circumference of the circle is " << circumference << endl << endl;
+return 0;
+}
