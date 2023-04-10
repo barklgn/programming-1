83b3ae3446c6e0689b12f64606b025244e61ba48
BARKLGN MULUGETA <65805233+barklgn@users.noreply.github.com>
2023-04-10 07:48:28 +0300

    Add files via upload

---
+++ program 3 number multiplayer.cpp
@@ -0,0 +1,20 @@
+﻿﻿/* This code is created by
+Name : BARKLGN MULUGETA
+Id : Ru 2141/14 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//Question 1 (a program that calculates the product of 3 numbers)
+cout << "the 1st program calculates the product of 3 numbers" << endl;
+int x,y,z,result;
+cout << "inter the three numbers to multiply" << endl;
+cin >> x >> y >> z;
+result = x*y*z;
+cout << "the product is " << result << endl << endl ;
+return 0;
+}
