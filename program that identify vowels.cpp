70ad69d117f20ccc399a598840f11d646cf517b5
BARKLGN MULUGETA <65805233+barklgn@users.noreply.github.com>
2023-04-17 12:48:05 +0300

    Add files via upload

---
+++ program that identify vowels.cpp
@@ -0,0 +1,31 @@
+﻿/* This code is created by
+Name : BARKLGN MULUGETA
+Id : Ru 2141/14 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//program that identifies vowels
+    char ch;
+cout << "enter the character to determine if it is vowel or not" << endl;
+cin >> ch;
+if (ch == 'a'){
+    cout << "it's vowel" << endl;
+}
+    else if (ch == 'i'){
+    
+    }
+    else if (ch == 'o'){
+        cout << "it's vowel" << endl;
+    }
+        else if (ch == 'u'){
+        cout << "it's vowel" << endl;
+    }
+        else if (ch == 'e'){
+        cout << "it's vowel" << endl;
+    }
+return 0;
+}
