1119d93f7e09fe97fe1a534b78b127e09aeaa117
BARKLGN MULUGETA <65805233+barklgn@users.noreply.github.com>
2023-04-18 09:07:40 +0300

    Add files via upload

---
+++ program that calculates grade.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+    
+    int score;
+    cout << "enter the score" << endl;
+    cin >> score;
+    
+    if(score >= 90){
+        cout << "it's A+" << endl;
+    }
+    if(score >= 85 && score < 90){
+        cout << "it's A" << endl;
+    }
+    if(score >= 80 && score < 85){
+        cout << "it's A-" << endl;
+    }
+    if(score >= 75 && score < 80){
+        cout << "it's B+" << endl;
+    }
+    if(score >= 70 && score < 75){
+        cout << "it's B" << endl;
+    }
+    if(score >= 65 && score < 70){
+        cout << "it's B-" << endl;
+    }
+    if(score >= 60 && score < 65){
+        cout << "it's C+" << endl;
+    }
+    if(score >= 50 && score < 60){
+        cout << "it's C" << endl;
+    }
+    if(score >= 45 && score < 50){
+        cout << "it's C-" << endl;
+    }
+    if(score >= 40 && score < 45){
+        cout << "it's D" << endl;
+    }
+    if(score < 45){
+        cout << "it's F / fail" << endl;
+    }
+    
+    return 0;
+}
